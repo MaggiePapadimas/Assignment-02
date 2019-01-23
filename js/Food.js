9b4befd98bfb02c16136972a242f2191f56e79ad
MaggiePapadimas <43021604+MaggiePapadimas@users.noreply.github.com>
2019-01-22 22:25:51 -0500

    completed - has array for multiple foods, move randomly, and have different colors

--- js/Food.js
+++ js/Food.js
@@ -10,10 +10,14 @@ class Food extends Agent {
   // Pass arguments on to the super() constructor (e.g. for Agent)
   // Also set a minimum and maximum size for this food object which it
   // will vary between when it resets
-  constructor(x,y,minSize,maxSize) {
-    super(x,y,random(minSize,maxSize),'#55cccc');
+  constructor(x,y,minSize,maxSize,vx,vy,maxSpeed, timer, color) {
+    super(x,y,random(minSize,maxSize),color);
     this.minSize = minSize;
     this.maxSize = maxSize;
+    this.vx = vx;
+    this.vy = vy;
+    this.maxSpeed = maxSpeed;
+    this.timer = timer;
   }
 
   // reset()
@@ -24,5 +28,34 @@ class Food extends Agent {
     this.x = random(0,width);
     this.y = random(0,height);
     this.size = random(this.minSize,this.maxSize);
+  }
+
+  update(){
+  this.velocity();
+  // moves the food
+  this.x += this.vx;
+  this.y += this.vy;
+  // makes the food stay in place
+  if (this.x < 0) {
+      this.x += width;
+  }
+    else if (this.x > width) {
+      this.x -= width;
+    }
+  if (this.y < 0) {
+    this.y += height;
+  }
+    else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
+  velocity() {
+    this.timer += 1;
+    if (this.timer >= 20){
+      this.vx = random(-this.maxSpeed, this.maxSpeed);
+      this.vy = random(-this.maxSpeed, this.maxSpeed);
+      this.timer=0;
+    }
   }
 }
--- js/script.js
+++ js/script.js
@@ -14,19 +14,24 @@ The player circle shrinks over time, but grows when it eats.
 
 // Constants for key quantities
 const AVATAR_MAX_SIZE = 64;
-const AVATAR_SIZE_LOSS_PER_FRAME = 1;
+const AVATAR_SIZE_LOSS_PER_FRAME = 0.3;
 const FOOD_MIN_SIZE = 5;
 const FOOD_MAX_SIZE = 100;
-
+const FOOD_VX = 5;
+const FOOD_VY = 5;
+const FOOD_MAX_SPEED = 10;
+const FOOD_TIMER = 0;
+const NUMBER_OF_FOOD = 30
 // Variables to store the two key objects
 let avatar;
-let food;
+
+
 
 
 // preload()
 //
 // Not needed
-
+let foods = []
 function preload() {
 
 }
@@ -39,7 +44,14 @@ function preload() {
 function setup() {
   createCanvas(windowWidth,windowHeight);
   avatar = new Avatar(mouseX,mouseY,AVATAR_MAX_SIZE,AVATAR_SIZE_LOSS_PER_FRAME)
-  food = new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE);
+//  food = new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE, FOOD_VX, FOOD_VY, FOOD_MAX_SPEED, FOOD_TIMER, foods);
+for(let i = 0; i < NUMBER_OF_FOOD; ++i){
+  console.log(color);
+  foods.push( new Food(random(0,width),random(0,height),FOOD_MIN_SIZE,FOOD_MAX_SIZE, FOOD_VX, FOOD_VY, FOOD_MAX_SPEED, FOOD_TIMER, random(40,255)));
+
+}
+
+
   noCursor();
 }
 
@@ -54,9 +66,17 @@ function draw() {
   background(0);
 
   avatar.update();
-  if (avatar.collide(food)) {
-    avatar.eat(food);
-  }
+
   avatar.display();
-  food.display();
+
+  for(let i = 0; i <foods.length;++i){
+
+    if (avatar.collide(foods[i])) {
+      avatar.eat(foods[i]);
+    }
+
+    foods[i].display();
+    foods[i].update();
+  }
+
 }
